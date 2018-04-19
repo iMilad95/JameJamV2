af0dd42d59e5fb2e0e6ff7ca0dcf24200a3f0228
miladtala95 <miladtala95@gmail.com>
2018-04-19 13:00:34 +0430

    Initial commit

---
+++ app/src/main/java/ir/i95milad/jamejam/android/jamejamv2/SplashActivity.java
@@ -0,0 +1,28 @@
+package ir.i95milad.jamejam.android.jamejamv2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+    @BindView(R.id.sign_in_btn) Button signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        });
+    }
+}
